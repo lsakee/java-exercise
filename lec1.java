86b5f0b3670b1c48bc068b7dbdb9c1a0c01dfd77
esak <lsls4868@gmail.com>
2021-11-18 00:05:09 +0900

    Add java File[lec1.java]

---
+++ lec1.java
@@ -0,0 +1,22 @@
+package tutorial;
+
+public class lec1 {
+    final static double PI = 3.141592;
+    final static int INT_M = 2147483647;
+    public static void main(String[] args){
+        int intT = 10;
+        int c = 2;
+        int b = 4;
+        int a = INT_M;
+        double douT = 150.5;
+        String strT = "esak";
+        System.out.println("c+b="+(c+b));
+        System.out.println("c-b="+(c-b));
+        System.out.println("c*b="+(c*b));
+        System.out.println("c/b="+(c/b));
+        System.out.println(intT);
+        System.out.println(douT);
+        System.out.println(strT);
+        System.out.println(intT*intT*PI);
+    }
+}
